33c61f80580312c2acc31f4055d095d8825f39df
silky <noonsilk+-noonsilk@gmail.com>
2018-07-17 10:40:49 +1000

    first version

---
+++ src/site.hs
@@ -0,0 +1,122 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+import qualified Data.Set as S
+import           Text.Pandoc.Options
+import           Control.Monad
+--------------------------------------------------------------------------------
+
+pandocMathCompiler =
+    let mathExtensions = [Ext_tex_math_dollars, Ext_latex_macros,
+                         Ext_backtick_code_blocks]
+        defaultExtensions = writerExtensions defaultHakyllWriterOptions
+        newExtensions = foldr S.insert defaultExtensions mathExtensions
+        writerOptions = defaultHakyllWriterOptions {
+                          writerExtensions = newExtensions,
+                          writerHTMLMathMethod = MathJax ""
+                        }
+    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+
+
+main :: IO ()
+main = hakyll $ do
+    match "images/**/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "files/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocMathCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocMathCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match (fromList ["links.html", "about.html"]) $ do
+        route idRoute
+        compile $ do
+            let linksCtx = defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate linksCtx
+                >>= loadAndApplyTemplate "templates/default.html" linksCtx
+                >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- (liftM (take 10)) $ recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "posts/*" "content"
+            renderAtom feedConf feedCtx posts
+
+
+feedConf :: FeedConfiguration
+feedConf = FeedConfiguration
+    { feedTitle         = "vandersilk.github.io"
+    , feedDescription   = "Website of Van der Silk"
+    , feedAuthorName    = "Noon van der Silk"
+    , feedAuthorEmail   = "noonsilk+-noonsilk@gmail.com"
+    , feedRoot          = "https://vandersilk.github.io"
+    }
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
